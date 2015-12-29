e0917e9143d94848b824fc0d3383518b174fe66b
Filipe Waitman <filwaitman@gmail.com>
2015-12-29 20:48:46 -0200

    WIP

--- jinja2_standalone_compiler.py
+++ jinja2_standalone_compiler.py
@@ -1,43 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import fnmatch
 import os
 
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
-from settings import INPUT_FOLDER, OUTPUT_FOLDER, OUTPUT_TEMPLATES, EXTRA_VARIABLES
+from settings import EXTRA_VARIABLES
 
 
-def compile_template(skeleton_path):
+def render_template(jinja_template):
     '''Returns a string with its generated content.
 
-    :param skeleton_path: template basis to be used to render final template.  Use relative path, (from INPUT_FOLDER on)
-    :type skeleton_path: str or unicode
+    :param jinja_template: template basis to be used to render final template.  Use relative path, (from INPUT_FOLDER on)
+    :type jinja_template: str or unicode
     :returns: formatted string
     :rtype: str or unicode
     '''
-    environment = Environment(loader=FileSystemLoader([INPUT_FOLDER]), trim_blocks=True, lstrip_blocks=True)
+    environment = Environment(loader=FileSystemLoader([os.path.dirname(jinja_template)]), trim_blocks=True, lstrip_blocks=True)
     environment.undefined = StrictUndefined
-    template = environment.get_template(skeleton_path)
+    template = environment.get_template(os.path.basename(jinja_template))
     return template.render(EXTRA_VARIABLES)
 
 
 def main():
-    for jinja_template in OUTPUT_TEMPLATES:
-        template_name, _ = os.path.splitext(jinja_template)
-        template_name = os.path.join(OUTPUT_FOLDER, '%s.html' % template_name)
+    current_dir = os.path.dirname(os.path.realpath(__file__))
 
-        if not os.path.exists(OUTPUT_FOLDER):
-            os.makedirs(OUTPUT_FOLDER)
+    jinja_templates = []
+    for root, dirnames, filenames in os.walk(current_dir):
+        for filename in fnmatch.filter(filenames, '*.jinja*'):
+            jinja_templates.append(os.path.join(root, filename))
 
-        with open(template_name, 'w') as f:
-            f.write(compile_template(jinja_template).encode('utf-8'))
+    for jinja_template in jinja_templates:
+        html_template, _ = os.path.splitext(jinja_template)
+        html_template = '{}.html'.format(html_template)
+
+        with open(html_template, 'w') as f:
+            f.write(render_template(jinja_template).encode('utf-8'))
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except Exception, e:
-        print 'ERROR: %s' % str(e)
-    else:
-        print 'Done!'
-    raw_input('Press any key to continue...')
+    main()
