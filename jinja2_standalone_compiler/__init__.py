9a19016a31b4f67955fcc3d9c37f0239aa60d271
Tom Bishop <tom.smart.bishop@gmail.com>
2016-09-01 18:22:14 +0900

    Refactored console output, now also using colors (via colorama) to keep better overview on the console output Added support for multiple settings files so that a template can be rendered multiple times with differnent settings (I'm using that to generate *.h and *.inl out of the same jinja file) Added shortcut options for --out (-o) and --settings(-s)

--- jinja2_standalone_compiler/__init__.py
+++ jinja2_standalone_compiler/__init__.py
@@ -4,13 +4,22 @@ import imp
 import os
 import re
 import sys
-
 import click
+from colorama import init, Fore, Style
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 from jinja2.defaults import (
     BLOCK_START_STRING, BLOCK_END_STRING, VARIABLE_START_STRING, VARIABLE_END_STRING, COMMENT_START_STRING,
     COMMENT_END_STRING, LINE_STATEMENT_PREFIX, LINE_COMMENT_PREFIX, NEWLINE_SEQUENCE, KEEP_TRAILING_NEWLINE
 )
+
+# Globals
+global_silent = False
+global_verbose = False
+
+
+def print_log(msg, verbose_msg=False):
+    if not global_silent and ((verbose_msg and global_verbose) or not verbose_msg):
+        print msg
 
 
 def render_template(jinja_template, extra_variables, output_options, jinja_environment):
@@ -34,21 +43,19 @@ def render_template(jinja_template, extra_variables, output_options, jinja_envir
     return template.render(extra_variables)
 
 
-def main(path, settings=None, out_path=None, verbose=False, silent=False):
+def main(path, out_path=None, settings=None):
     extra_variables = {}
     ignore_jinja_templates = []
     output_options = {}
     jinja_environment = {}
     if settings:
         extra_variables = getattr(settings, 'EXTRA_VARIABLES', {})
-        ignore_jinja_templates = getattr(
-            settings, 'IGNORE_JINJA_TEMPLATES', [])
+        ignore_jinja_templates = getattr(settings, 'IGNORE_JINJA_TEMPLATES', [])
         output_options = getattr(settings, 'OUTPUT_OPTIONS', {})
         jinja_environment = getattr(settings, 'JINJA_ENVIRONMENT', {})
 
     if os.path.isdir(path):
-        if not silent:
-            print 'Looking for jinja templates in:', path
+        print_log('  Looking for jinja templates in: {}{}'.format(Fore.MAGENTA, path))
         jinja_templates = []
         for root, dirnames, filenames in os.walk(path):
             for filename in fnmatch.filter(filenames, '*.jinja*'):
@@ -56,16 +63,15 @@ def main(path, settings=None, out_path=None, verbose=False, silent=False):
     else:
         jinja_templates = [path, ]  # path is just a file, actually
 
-    if not silent:
-        print 'Jinja files found:', len(jinja_templates)
+    print_log('  Jinja files found: {}{}'.format(Fore.MAGENTA, len(jinja_templates)))
+
     for jinja_template in jinja_templates:
-        if not silent:
-            print 'Processing:', jinja_template
+        print_log('   Processing:' + Fore.MAGENTA + jinja_template)
+
         skip = False
         for jinja_template_to_be_ignored in ignore_jinja_templates:
             if re.match(jinja_template_to_be_ignored, jinja_template):
-                if not silent:
-                    print '  Skipping:', jinja_template
+                print_log('    Skipping: ' + Fore.YELLOW + Style.BRIGHT + jinja_template)
                 skip = True
                 break
 
@@ -80,26 +86,26 @@ def main(path, settings=None, out_path=None, verbose=False, silent=False):
                 try:
                     os.makedirs(template_dir)
                 except:
-                    raise IOError(
-                        u'Cannot create sub output directory: {}'.format(template_dir))
+                    raise IOError(u'Cannot create sub output directory: {}'.format(template_dir))
+
             template_file, _ = os.path.splitext(template_file)
         else:
             template_file, _ = os.path.splitext(jinja_template)
 
+        template_file = os.path.abspath(template_file)
+
         if output_options.get('remove_double_extension', False):
             template_file, _ = os.path.splitext(template_file)
-        template_file = '{}{}'.format(
-            template_file, output_options.get('extension', '.html'))
 
-        if not silent:
-            print '  Creating:', template_file
+        template_file = '{}{}'.format(template_file, output_options.get('extension', '.html'))
+
+        print_log('    Creating: ' + Fore.CYAN + template_file)
+
         try:
             with open(template_file, 'w') as f:
-                if not silent and verbose:
-                    print '    Base template      :', jinja_template
-                    print '      EXTRA_VARIABLES  :', extra_variables
-                    print '      OUTPUT_OPTIONS   :', output_options
-                    print '      JINJA_ENVIRONMENT:', jinja_environment
+                print_log('     EXTRA_VARIABLES  : {}'.format(extra_variables), True)
+                print_log('     OUTPUT_OPTIONS   : {}'.format(output_options), True)
+                print_log('     JINJA_ENVIRONMENT: {}'.format(jinja_environment), True)
                 f.write(render_template(
                     jinja_template,
                     extra_variables=extra_variables,
@@ -110,34 +116,47 @@ def main(path, settings=None, out_path=None, verbose=False, silent=False):
             os.unlink(template_file)
             raise
 
-    if not silent:
-        print 'Done!'
-
 
 @click.command()
 @click.argument('path', type=click.Path(exists=True))
-@click.option('--settings', default=None, help='Settings file to use.')
-@click.option('--out', type=click.Path(exists=False, file_okay=False, dir_okay=True), default=None, help='Output path to use.')
+@click.option('--settings', '-s', default=None, multiple=True, help='Settings file to use.')
+@click.option('--out', '-o', type=click.Path(exists=False, file_okay=False, dir_okay=True), default=None, help='Output path to use.')
 @click.option('--verbose', is_flag=True, default=False, help='Detailed command line output')
 @click.option('--silent', is_flag=True, default=False, help='Suppress command line output')
 def main_command(path, settings=None, out=None, verbose=False, silent=False):
+    init(autoreset=True)  # Init colorama
     current_dir = os.getcwd()
 
-    if settings:
-        settings_file = os.path.join(current_dir, settings)
-        if not os.path.exists(settings_file):
-            raise IOError(u'Settings file not found: {}'.format(settings_file))
-
-        sys.path.insert(0, '')
-        settings = imp.load_source(current_dir, settings)
-
-    current_dir = os.path.join(current_dir, path)
+    global_silent = silent
+    global_verbose = verbose
 
     if out and not os.path.exists(out):
+        out = os.path.normpath(out)
         try:
             os.makedirs(out)
         except:
             raise IOError(u'Cannot create output directory: {}'.format(out))
 
-    main(settings=settings, path=current_dir,
-         out_path=out, verbose=verbose, silent=silent)
+    if settings:
+        if not silent:
+            print_log('{}Number of specified setting files: {}'.format(Fore.GREEN, len(settings)))
+        for setting in settings:
+            settings_file = os.path.normpath(os.path.join(current_dir, setting))
+            if not os.path.exists(settings_file):
+                raise IOError(u'Settings file not found: {}'.format(settings_file))
+            else:
+                if not silent:
+                    print_log(' Using settings file: ' + Fore.CYAN + settings_file)
+            sys.path.insert(0, '')
+            setting = imp.load_source(current_dir, setting)
+            work_dir = os.path.normpath(os.path.join(current_dir, path))
+
+            main(work_dir, out, setting)
+
+            print_log(Fore.GREEN + ' Done.')
+    else:
+        work_dir = os.path.join(current_dir, path)
+
+        main(work_dir, out)
+
+    print_log(Fore.GREEN + Style.BRIGHT + 'All done.')
