0590e9982b790550fb9c1530ee88afb11ac4fc4c
Tom Bishop <tom.smart.bishop@gmail.com>
2016-09-01 10:08:15 +0900

    Typo fix and linewidth 130

--- jinja2_standalone_compiler/__init__.py
+++ jinja2_standalone_compiler/__init__.py
@@ -16,28 +16,18 @@ from jinja2.defaults import (
 def render_template(jinja_template, extra_variables, output_options, jinja_environment):
     environment = Environment(
         loader=FileSystemLoader([os.path.dirname(jinja_template)]),
-        block_start_string=jinja_environment.get(
-            'BLOCK_START_STRING', BLOCK_START_STRING),
-        block_end_string=jinja_environment.get(
-            'BLOCK_END_STRING', BLOCK_END_STRING),
-        variable_start_string=jinja_environment.get(
-            'VARIABLE_START_STRING', VARIABLE_START_STRING),
-        variable_end_string=jinja_environment.get(
-            'VARIABLE_END_STRING', VARIABLE_END_STRING),
-        comment_start_string=jinja_environment.get(
-            'COMMENT_START_STRING', COMMENT_START_STRING),
-        comment_end_string=jinja_environment.get(
-            'COMMENT_END_STRING', COMMENT_END_STRING),
-        line_statement_prefix=jinja_environment.get(
-            'LINE_STATEMENT_PREFIX', LINE_STATEMENT_PREFIX),
-        line_comment_prefix=jinja_environment.get(
-            'LINE_COMMENT_PREFIX', LINE_COMMENT_PREFIX),
+        block_start_string=jinja_environment.get('BLOCK_START_STRING', BLOCK_START_STRING),
+        block_end_string=jinja_environment.get('BLOCK_END_STRING', BLOCK_END_STRING),
+        variable_start_string=jinja_environment.get('VARIABLE_START_STRING', VARIABLE_START_STRING),
+        variable_end_string=jinja_environment.get('VARIABLE_END_STRING', VARIABLE_END_STRING),
+        comment_start_string=jinja_environment.get('COMMENT_START_STRING', COMMENT_START_STRING),
+        comment_end_string=jinja_environment.get('COMMENT_END_STRING', COMMENT_END_STRING),
+        line_statement_prefix=jinja_environment.get('LINE_STATEMENT_PREFIX', LINE_STATEMENT_PREFIX),
+        line_comment_prefix=jinja_environment.get('LINE_COMMENT_PREFIX', LINE_COMMENT_PREFIX),
         trim_blocks=jinja_environment.get('TRIM_BLOCKS', True),
         lstrip_blocks=jinja_environment.get('LSTRIP_BLOCKS', True),
-        newline_sequence=jinja_environment.get(
-            'NEWLINE_SEQUENCE', NEWLINE_SEQUENCE),
-        keep_trailing_newline=jinja_environment.get(
-            'KEEP_TRAILING_NEWLINE', KEEP_TRAILING_NEWLINE)
+        newline_sequence=jinja_environment.get('NEWLINE_SEQUENCE', NEWLINE_SEQUENCE),
+        keep_trailing_newline=jinja_environment.get('KEEP_TRAILING_NEWLINE', KEEP_TRAILING_NEWLINE)
     )
     environment.undefined = StrictUndefined
     template = environment.get_template(os.path.basename(jinja_template))
@@ -91,7 +81,7 @@ def main(path, settings=None, out_path=None, verbose=False, silent=False):
                     os.makedirs(template_dir)
                 except:
                     raise IOError(
-                        u'Cannot create sub ouput directory: {}'.format(template_dir))
+                        u'Cannot create sub output directory: {}'.format(template_dir))
             template_file, _ = os.path.splitext(template_file)
         else:
             template_file, _ = os.path.splitext(jinja_template)
@@ -147,7 +137,7 @@ def main_command(path, settings=None, out=None, verbose=False, silent=False):
         try:
             os.makedirs(out)
         except:
-            raise IOError(u'Cannot create ouput directory: {}'.format(out))
+            raise IOError(u'Cannot create output directory: {}'.format(out))
 
     main(settings=settings, path=current_dir,
          out_path=out, verbose=verbose, silent=silent)
